c10a309f5f655afd33af8d95df19a087332873fc
Igor Konovalov <konovalov.avp@gmail.com>
2017-03-29 10:28:47 +0300

    fixed some bugs code cleanup 2

--- src/index.js
+++ src/index.js
@@ -6,13 +6,15 @@ let r, x, y, fi, deg;
 const inputN = document.getElementById('n');
 const inputD = document.getElementById('d');
 const inputMaurer = document.getElementById('maurer');
-const inputSize= document.getElementById('size');
+const inputSize = document.getElementById('size');
 const inputRotate = document.getElementById('rotate');
 const inputGG= document.getElementById('gg');
-const arrayInputs = [inputN, inputD, inputMaurer,inputSize,inputRotate ,inputGG];
+const arrayInputs = [inputN, inputD, inputMaurer, inputSize, inputRotate, inputGG];
 const checkRose = document.getElementById('showRose');
 const checkMaurer = document.getElementById('showMaurer');
 const arrayCheck = [checkRose, checkMaurer];
+
+// INITIAL CONST
 
 let n = 20;
 let d = 2;
@@ -20,7 +22,7 @@ let k = n/d;
 let maurer = 71; // 0-360
 let size = 300;
 let rotate = 0;
-let gg = 0; //Guido Grandi
+let gg = 0; // Guido Grandi
 
 const xStart = canvas.width/2;
 const yStart = canvas.height/2;
@@ -59,19 +61,8 @@ function draw(n, d, maurer, k,size, rotate,gg) {
 // pre-render
 draw(n, d, maurer, k,size,rotate,gg);
 
-// buttons, range etc..
+// VISUAL SETUP
 
-const button = document.getElementById('generate');
-button.addEventListener('click', () => {
-  n = inputN.value;
-  d = inputD.value;
-  maurer = inputMaurer.value;
-  rotate = inputRotate.value;
-  gg = inputGG.value;
-  size = inputSize.value;
-  k = n / d;
-  draw(n, d, maurer, k,size,rotate,gg);
-})
 
 // sliders setup
 
@@ -94,6 +85,8 @@ rangeMaurerValue.innerHTML = rangeMaurer.value;
 const rangeRotateValue = document.getElementById('rangeRotateValue');
 const rangeRotate = document.getElementById('rangeRotate');
 elementsArray.push(rangeRotate);
+rangeRotateValue.innerHTML = rangeRotate.value;
+
 
 const rangeGGValue = document.getElementById('rangeGGValue');
 const rangeGG = document.getElementById('rangeGG');
@@ -108,6 +101,13 @@ rangeSizeValue.innerHTML = rangeSize.value;
 // change events handling
 elementsArray.forEach((element)=>{
   element.addEventListener('mousemove', () => {
+    rangeNValue.innerHTML = rangeN.value;
+    rangeDValue.innerHTML = rangeD.value;
+    rangeMaurerValue.innerHTML = rangeMaurer.value;
+    rangeGGValue.innerHTML = rangeGG.value;
+    rangeSizeValue.innerHTML = rangeSize.value;
+    rangeRotateValue.innerHTML = rangeRotate.value;
+
     n = rangeN.value;
     d = rangeD.value;
     maurer = rangeMaurer.value;
@@ -121,6 +121,13 @@ elementsArray.forEach((element)=>{
 
 elementsArray.forEach((element)=>{
   element.addEventListener('touchmove', () => {
+    rangeNValue.innerHTML = rangeN.value;
+    rangeDValue.innerHTML = rangeD.value;
+    rangeMaurerValue.innerHTML = rangeMaurer.value;
+    rangeGGValue.innerHTML = rangeGG.value;
+    rangeSizeValue.innerHTML = rangeSize.value;
+    rangeRotateValue.innerHTML = rangeRotate.value;
+
     n = rangeN.value;
     d = rangeD.value;
     maurer = rangeMaurer.value;
@@ -138,6 +145,8 @@ arrayInputs.forEach((element)=> {
     rangeDValue.innerHTML = rangeD.value = inputD.value;
     rangeMaurerValue.innerHTML = rangeMaurer.value = inputMaurer.value;
 	  rangeGGValue.innerHTML = rangeGG.value = inputGG.value;
+    rangeSizeValue.innerHTML = rangeSize.value = inputSize.value;
+    rangeRotateValue.innerHTML = rangeRotate.value = inputRotate.value;
   })
 })
 
@@ -145,10 +154,22 @@ arrayCheck.forEach((checkbox) => {
   checkbox.addEventListener('change', () => {
     n = rangeN.value;
     d = rangeD.value;
-	rotate = rangeRotate.value;
+    rotate = rangeRotate.value;
     maurer = rangeMaurer.value;
-	gg = rangeGG.value;
+    gg = rangeGG.value;
     k = n / d;
     draw(n, d, maurer, k, rotate,gg);
   })
+})
+
+const button = document.getElementById('generate');
+button.addEventListener('click', () => {
+  n = inputN.value;
+  d = inputD.value;
+  maurer = inputMaurer.value;
+  rotate = inputRotate.value;
+  gg = inputGG.value;
+  size = inputSize.value;
+  k = n / d;
+  draw(n, d, maurer, k,size,rotate,gg);
 })
