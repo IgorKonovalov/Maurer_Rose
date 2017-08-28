ff1b7739e543810854acc54784cbde397b0c2285
IgorKonovalov <konovalov.avp@gmail.com>
2017-08-28 15:45:55 +0300

    refactoring

--- src/draw.js
+++ src/draw.js
@@ -1,62 +1,72 @@
 // INITIAL SETUP
 
-let r, x, y, fi, deg;
+let r, x, y, fi, deg
 
-let n = 20;
-let d = 2;
-let k = n/d;
-let maurer = 71; // 0-360
-let size = 300;
-let rotate = 0;
-let gg = 0; // Guido Grandi
+let n = 2
+let d = 20
+let k = n / d
+let maurer = 71 // 0-360
+let size = 300
+let rotate = 0
+let gg = 0 // Guido Grandi
 
-const xStart = canvas.width/2;
-const yStart = canvas.height/2;
+inputN.value = rangeNValue.innerHTML = rangeN.value = 2
+inputD.value = rangeDValue.innerHTML = rangeD.value = 20
+inputMaurer.value = rangeMaurerValue.innerHTML = rangeMaurer.value = 71
+inputSize.value = rangeSizeValue.innerHTML = rangeSize.value = 300
+inputRotate.value = rangeRotateValue.innerHTML = rangeRotate.value = 0
+inputGG.value = rangeGGValue.innerHTML = rangeGG.value = 0
 
-cx.lineCap = 'round';
+const xStart = canvas.width / 2
+const yStart = canvas.height / 2
 
-function draw(n, d, maurer, k,size, rotate,gg) {
-  cx.clearRect(0, 0, canvas.width, canvas.height);
+cx.lineCap = 'round'
+
+function draw(n, d, maurer, k, size, rotate, gg) {
+  cx.clearRect(0, 0, canvas.width, canvas.height)
   // connecting rose on angle
   if (checkMaurer.checked) {
     for (let i = 0; i < 3600; i++) {
-      if (i < 2000) {
-        cx.strokeStyle = `hsl(0, 0%, 50%)`
+      if (colorize.checked) {
+        if (i < 2000) {
+          cx.strokeStyle = `hsl(0, 0%, 50%)`
+        } else {
+          cx.strokeStyle = `hsl(${i}, 100%, 50%)`
+        }
       } else {
-        cx.strokeStyle = `hsl(${i}, 100%, 50%)`
+        cx.strokeStyle = 'white'
       }
-      cx.lineWidth = .5
-      cx.beginPath();
-      fi = (maurer * i) * Math.PI / 180;
-      r = Math.sin(-k * fi-rotate) * (size) + Math.round(gg);
-      x = xStart + r * Math.cos(fi);
-      y = yStart + r * Math.sin(fi);
+      cx.lineWidth = 0.5
+      cx.beginPath()
+      fi = maurer * i * Math.PI / 180
+      r = Math.sin(-k * fi - rotate) * size + Math.round(gg)
+      x = xStart + r * Math.cos(fi)
+      y = yStart + r * Math.sin(fi)
       if (i === 0) {
         cx.moveTo(x, y)
       } else {
         cx.moveTo(xPrev, yPrev)
       }
-      xPrev = x;
-      yPrev = y;
-      cx.lineTo(x, y);
-      cx.stroke();
+      xPrev = x
+      yPrev = y
+      cx.lineTo(x, y)
+      cx.stroke()
     }
-
   }
   // drawing rose
   if (checkRose.checked) {
-    cx.beginPath();
-    cx.strokeStyle = 'red';
-    cx.lineWidth = .1
+    cx.beginPath()
+    cx.strokeStyle = 'red'
+    cx.lineWidth = 0.1
     for (let a = 0; a < 3600 * Math.ceil(d); a++) {
-      deg = a * Math.PI / 180;
-      r = Math.sin(-k * deg-rotate) * (size) + Math.round(gg);
-      x = xStart + r * Math.cos(deg);
-      y = xStart + r * Math.sin(deg);
-      cx.lineTo(x, y);
+      deg = a * Math.PI / 180
+      r = Math.sin(-k * deg - rotate) * size + Math.round(gg)
+      x = xStart + r * Math.cos(deg)
+      y = xStart + r * Math.sin(deg)
+      cx.lineTo(x, y)
     }
-    cx.stroke();
+    cx.stroke()
   }
 }
 // pre-render
-draw(n, d, maurer, k,size,rotate,gg);
+draw(n, d, maurer, k, size, rotate, gg)
--- src/index.js
+++ src/index.js
@@ -1,111 +1,125 @@
-const canvas = document.getElementById('rose');
-const cx = canvas.getContext('2d');
+const canvas = document.getElementById('rose')
+const cx = canvas.getContext('2d')
 
 // INPUTS
 
-const inputN = document.getElementById('n');
-const inputD = document.getElementById('d');
-const inputMaurer = document.getElementById('maurer');
-const inputSize = document.getElementById('size');
-const inputRotate = document.getElementById('rotate');
-const inputGG= document.getElementById('gg');
+const inputN = document.getElementById('n')
+const inputD = document.getElementById('d')
+const inputMaurer = document.getElementById('maurer')
+const inputSize = document.getElementById('size')
+const inputRotate = document.getElementById('rotate')
+const inputGG = document.getElementById('gg')
 
-const arrayInputs = [inputN, inputD, inputMaurer, inputSize, inputRotate, inputGG];
+const arrayInputs = [
+  inputN,
+  inputD,
+  inputMaurer,
+  inputSize,
+  inputRotate,
+  inputGG
+]
 
 // CHECKBOXES
 
-const checkRose = document.getElementById('showRose');
-const checkMaurer = document.getElementById('showMaurer');
+const checkRose = document.getElementById('showRose')
+const checkMaurer = document.getElementById('showMaurer')
+const colorize = document.getElementById('colorize')
 
-const arrayCheck = [checkRose, checkMaurer];
+const arrayCheck = [checkRose, checkMaurer, colorize]
 
 // SLIDERS
 
-const rangeN = document.getElementById('rangeN');
-const rangeD = document.getElementById('rangeD');
-const rangeMaurer = document.getElementById('rangeMaurer');
-const rangeRotate = document.getElementById('rangeRotate');
-const rangeGG = document.getElementById('rangeGG');
-const rangeSize = document.getElementById('rangeSize');
+const rangeN = document.getElementById('rangeN')
+const rangeD = document.getElementById('rangeD')
+const rangeMaurer = document.getElementById('rangeMaurer')
+const rangeRotate = document.getElementById('rangeRotate')
+const rangeGG = document.getElementById('rangeGG')
+const rangeSize = document.getElementById('rangeSize')
 
-let elementsArray = [rangeN, rangeD, rangeMaurer, rangeRotate, rangeGG, rangeSize]
+let elementsArray = [
+  rangeN,
+  rangeD,
+  rangeMaurer,
+  rangeRotate,
+  rangeGG,
+  rangeSize
+]
 
 // VALUE OUTPUT
 
-const rangeNValue = document.getElementById('rangeNValue');
-const rangeDValue = document.getElementById('rangeDValue');
-const rangeMaurerValue = document.getElementById('rangeMaurerValue');
-const rangeRotateValue = document.getElementById('rangeRotateValue');
-const rangeGGValue = document.getElementById('rangeGGValue');
-const rangeSizeValue = document.getElementById('rangeSizeValue');
+const rangeNValue = document.getElementById('rangeNValue')
+const rangeDValue = document.getElementById('rangeDValue')
+const rangeMaurerValue = document.getElementById('rangeMaurerValue')
+const rangeRotateValue = document.getElementById('rangeRotateValue')
+const rangeGGValue = document.getElementById('rangeGGValue')
+const rangeSizeValue = document.getElementById('rangeSizeValue')
 
 // INITIAL OUTPUT SETUP
 
-rangeNValue.innerHTML = rangeN.value;
-rangeDValue.innerHTML = rangeD.value;
-rangeMaurerValue.innerHTML = rangeMaurer.value;
-rangeRotateValue.innerHTML = rangeRotate.value;
-rangeGGValue.innerHTML = rangeGG.value;
-rangeSizeValue.innerHTML = rangeSize.value;
+rangeNValue.innerHTML = rangeN.value
+rangeDValue.innerHTML = rangeD.value
+rangeMaurerValue.innerHTML = rangeMaurer.value
+rangeRotateValue.innerHTML = rangeRotate.value
+rangeGGValue.innerHTML = rangeGG.value
+rangeSizeValue.innerHTML = rangeSize.value
 
 // EVENT HANDLERS
 
 function addListenerMulti(element, listeners, fn) {
-  listeners.split(' ').forEach(event => element.addEventListener(event, fn, false));
+  listeners
+    .split(' ')
+    .forEach(event => element.addEventListener(event, fn, false))
 }
 
 elementsArray.forEach(element => {
   addListenerMulti(element, 'mousemove touchmove', () => {
-    inputN.value = rangeNValue.innerHTML = rangeN.value;
-    inputD.value = rangeDValue.innerHTML = rangeD.value;
-    inputMaurer.value = rangeMaurerValue.innerHTML = rangeMaurer.value;
-    inputGG.value = rangeGGValue.innerHTML = rangeGG.value;
-    inputSize.value = rangeSizeValue.innerHTML = rangeSize.value;
-    inputRotate.value = rangeRotateValue.innerHTML = rangeRotate.value;
+    inputN.value = rangeNValue.innerHTML = rangeN.value
+    inputD.value = rangeDValue.innerHTML = rangeD.value
+    inputMaurer.value = rangeMaurerValue.innerHTML = rangeMaurer.value
+    inputGG.value = rangeGGValue.innerHTML = rangeGG.value
+    inputSize.value = rangeSizeValue.innerHTML = rangeSize.value
+    inputRotate.value = rangeRotateValue.innerHTML = rangeRotate.value
 
-    n = rangeN.value;
-    d = rangeD.value;
-    maurer = rangeMaurer.value;
-  	size = rangeSize.value;
-  	rotate = rangeRotate.value;
-  	gg = rangeGG.value;
-    k = n / d;
-    draw(n, d, maurer, k, size, rotate, gg);
-  });
-});
-
-arrayInputs.forEach((element)=> {
-  element.addEventListener('keyup', () => {
-    rangeNValue.innerHTML = rangeN.value = inputN.value;
-    rangeDValue.innerHTML = rangeD.value = inputD.value;
-    rangeMaurerValue.innerHTML = rangeMaurer.value = inputMaurer.value;
-	  rangeGGValue.innerHTML = rangeGG.value = inputGG.value;
-    rangeSizeValue.innerHTML = rangeSize.value = inputSize.value;
-    rangeRotateValue.innerHTML = rangeRotate.value = inputRotate.value;
+    n = rangeN.value
+    d = rangeD.value
+    maurer = rangeMaurer.value
+    size = rangeSize.value
+    rotate = rangeRotate.value
+    gg = rangeGG.value
+    k = n / d
+    draw(n, d, maurer, k, size, rotate, gg)
   })
 })
 
-arrayCheck.forEach((checkbox) => {
+arrayInputs.forEach(element => {
+  element.addEventListener('input', () => {
+    rangeNValue.innerHTML = rangeN.value = inputN.value
+    rangeDValue.innerHTML = rangeD.value = inputD.value
+    rangeMaurerValue.innerHTML = rangeMaurer.value = inputMaurer.value
+    rangeGGValue.innerHTML = rangeGG.value = inputGG.value
+    rangeSizeValue.innerHTML = rangeSize.value = inputSize.value
+    rangeRotateValue.innerHTML = rangeRotate.value = inputRotate.value
+
+    n = rangeN.value
+    d = rangeD.value
+    maurer = rangeMaurer.value
+    size = rangeSize.value
+    rotate = rangeRotate.value
+    gg = rangeGG.value
+    k = n / d
+    draw(n, d, maurer, k, size, rotate, gg)
+  })
+})
+
+arrayCheck.forEach(checkbox => {
   checkbox.addEventListener('change', () => {
-    n = inputN.value;
-    d = inputD.value;
-    maurer = inputMaurer.value;
-    rotate = inputRotate.value;
-    gg = inputGG.value;
-    size = inputSize.value;
-    k = n / d;
-    draw(n, d, maurer, k,size,rotate,gg);
+    n = inputN.value
+    d = inputD.value
+    maurer = inputMaurer.value
+    rotate = inputRotate.value
+    gg = inputGG.value
+    size = inputSize.value
+    k = n / d
+    draw(n, d, maurer, k, size, rotate, gg)
   })
-})
-
-const button = document.getElementById('generate');
-button.addEventListener('click', () => {
-  n = inputN.value;
-  d = inputD.value;
-  maurer = inputMaurer.value;
-  rotate = inputRotate.value;
-  gg = inputGG.value;
-  size = inputSize.value;
-  k = n / d;
-  draw(n, d, maurer, k,size,rotate,gg);
 })
